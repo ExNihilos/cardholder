c4bdf7a47d8f9234208cc9ad44d6b7d9987ce777
Svyatoslav Kulkov <slava.kulkov.1998@mail.ru>
2019-03-21 00:03:54 +0700

    Initial Commit

---
+++ app/src/main/java/info/goodline/cardholder/Card.kt
@@ -0,0 +1,20 @@
+package info.goodline.cardholder
+
+class Card(_name: String, _id: Int, _ref: String)
+{
+    var name: String = "Карта"
+    var id: Int = 1
+    var ref: String = "https://"
+    val cardInfo: String
+        get() {return " Карта \"$name\"  \n\n id:  $id  \n\n Фото: $ref"}
+
+    init
+    {
+        this.name = _name
+        this.id = _id
+        this.ref = _ref
+    }
+
+
+
+}
